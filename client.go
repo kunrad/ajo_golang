5f75d8613027200ffdb2b5a0aef8261b14f01dc4
kunny_kobo <chibuike@kobo360.com>
2021-01-15 15:30:30 +0100

    golang client

---
+++ client.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	for {
+		resp, err := http.Get("http://nodebackend.ajo.svc.cluster.local:3000")
+		if err != nil {
+			fmt.Printf( "failed to reach server: %v\n", err)
+			os.Exit(1)
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		fmt.Printf("Response from server: %v\n", string(body))
+		resp.Body.Close()
+		if err != nil {
+			fmt.Printf( "error: %v\n", err)
+			os.Exit(1)
+		}
+
+		time.Sleep(10 * time.Second)
+	}
+}
+
+
+
+
